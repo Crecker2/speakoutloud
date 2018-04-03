e1daeaf7f2886bf380f7c18e89cb06af4e14e310
Crecker2 <joshua.fayer@gmail.com>
2018-04-02 22:49:23 -0400

    upload site

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal();
+// sr.reveal('.cell', {origin: 'right', distance: '200px',});
+
+sr.reveal('.albumart', {origin: 'bottom', distance: '50px'},100);
+sr.reveal('.mike', {origin: 'bottom', distance: '100px'});
+sr.reveal('.material-icons',200)
